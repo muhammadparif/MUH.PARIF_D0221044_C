14c8d87b38dffaf278cde98091568ca5f1c44d6a
muhammadparif <muhammadparif028@gmail.com>
2022-04-15 10:52:24 +0800

    belajar java

---
+++ day55_java_reflection_attributes.java
@@ -0,0 +1,20 @@
+package com.tutorial;
+
+public class day55_java_reflection_attributes {
+
+    public static void main(String[] args) {
+        Class student = Student.class;
+        Method[] methods = student.getDeclaredMethods();
+
+        ArrayList<String> methodList = new ArrayList<>();
+        for (Method method : methods) {
+            methodList.add(method.getName());
+        }
+
+        Collections.sort(methodList);
+        for (String name : methodList) {
+            System.out.println(name);
+        }
+    }
+
+}
